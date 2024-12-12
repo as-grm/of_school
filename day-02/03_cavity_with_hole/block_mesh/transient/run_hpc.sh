267e702261fa6bfadcb725e812f3a2c1aac35951
Aleksander  Sandro GRM <aleksander.grm@fpp.uni-lj.si>
2024-12-12 10:49:36 +0100

    update

--- day-02/03_cavity_with_hole/block_mesh/transient/run_hpc.sh
+++ day-02/03_cavity_with_hole/block_mesh/transient/run_hpc.sh
@@ -9,16 +9,7 @@ module purge
 module load OpenFOAM
 source $FOAM_BASH
 
-cd ${0%/*} || exit 1    # Run from this directory
-
-# Source tutorial run functions
-. $WM_PROJECT_DIR/bin/tools/RunFunctions
-
-# decompose the case (number of decompositions is equal to --ntasks)
-runApplication decomposePar
-
 # run parallel
-echo "Start $(getApplication) in parallel. Log is written in case/log.$(getApplication)!"
-srun --mpi=pmix  $(getApplication) -parallel 
+srun --mpi=pmix foamRun -parallel 
 
 # Check the running process with: tail -f case/log.$(getApplication)
--- day-02/03_cavity_with_hole/block_mesh/transient/run_local.sh
+++ day-02/03_cavity_with_hole/block_mesh/transient/run_local.sh
@@ -2,10 +2,7 @@
 cd ${0%/*} || exit 1    # Run from this directory
 
 # Source tutorial clean functions
-. $WM_PROJECT_DIR/bin/tools/RunFunctions
-
-# decompose the case (number of decompositions is equal to --ntasks)
-runApplication decomposePar
+source $WM_PROJECT_DIR/bin/tools/RunFunctions
 
 # run parallel
 echo "Start $(getApplication) in parallel. Log is written in case/log.$(getApplication)!"
