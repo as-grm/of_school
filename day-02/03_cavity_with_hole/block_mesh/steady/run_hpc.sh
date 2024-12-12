6448b3343aef6b5d27230c3efa5fbe2063841e5c
Aleksander  Sandro GRM <aleksander.grm@fpp.uni-lj.si>
2024-12-12 09:29:59 +0100

    Added missing decomposePar in run_hpc.sh!

--- day-02/03_cavity_with_hole/block_mesh/steady/run_hpc.sh
+++ day-02/03_cavity_with_hole/block_mesh/steady/run_hpc.sh
@@ -14,8 +14,11 @@ cd ${0%/*} || exit 1    # Run from this directory
 # Source tutorial run functions
 . $WM_PROJECT_DIR/bin/tools/RunFunctions
 
+# decompose the case (number of decompositions is equal to --ntasks)
+runApplication decomposePar
+
 # run parallel
 echo "Start $(getApplication) in parallel. Log is written in case/log.$(getApplication)!"
-srun --mpi=pmix  $(getApplication) -parallel 
+srun --mpi=pmix  $(getApplication) -parallel > log.$(getApplication)
 
 # Check the running process with: tail -f case/log.$(getApplication)
--- day-02/03_cavity_with_hole/gmsh/transient/run_hpc.sh
+++ day-02/03_cavity_with_hole/gmsh/transient/run_hpc.sh
@@ -14,6 +14,9 @@ cd ${0%/*} || exit 1    # Run from this directory
 # Source tutorial run functions
 . $WM_PROJECT_DIR/bin/tools/RunFunctions
 
+# decompose the case (number of decompositions is equal to --ntasks)
+runApplication decomposePar
+
 # run parallel
 echo "Start $(getApplication) in parallel. Log is written in case/log.$(getApplication)!"
 srun --mpi=pmix  $(getApplication) -parallel 
